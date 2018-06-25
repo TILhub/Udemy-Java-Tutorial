d3695b1a08e12c3da1e3d80c2eeaebf62d480f7b
Sahil Malik <sahilmalikroorkee@gmail.com>
2018-06-25 17:20:53 +0530

    Add files via upload

---
+++ Lecture24.java
@@ -0,0 +1,18 @@
+class Lecture24{
+	public static void main(String s[]){
+
+		String str="";
+		str+="Hello";
+		str+=" ";
+		str+="dear";
+
+
+		StringBuilder sb=new StringBuilder("");
+		sb.append("Hie! ");
+		sb.append("My Name Is Dilgeeta");
+
+		System.out.println(str);
+		System.out.println(sb.toString());
+
+	}
+}	
